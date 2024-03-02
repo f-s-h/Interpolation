cb192ee85936b7ab44352dfa287aa8ebdcd125e8
f-s-h <fabianheinrich02@gmail.com>
2024-03-02 15:33:13 +0100

    Changed restriction of parameters and formatting

--- src/interpolation/InterpolationMethod.java
+++ src/interpolation/InterpolationMethod.java
@@ -7,26 +7,27 @@ public abstract class InterpolationMethod {
     /**
      * Leftmost x-value of the grid
      */
-    private double a;
+    protected double a;
     /**
      * Rightmost x-value of the grid
      */
-    private double b;
+    protected double b;
     /**
      * Number of intervals
      */
-    private int n;
+    protected int n;
     /**
      * Distance between two supporting points in x direction (width of intervals)
      */
-    private double h;
+    protected double h;
     /**
      * Set of given y-Values, used to create the interpolation function
      */
-    private double[] y;
+    protected double[] y;
 
     /**
      * Evaluates the interpolation function in the point z
+     *
      * @param z: x-Value, in which the interpolation function should be applied
      * @return y-Value: the result of the interpolation in point z
      */
@@ -42,6 +43,7 @@ public abstract class InterpolationMethod {
 
     /**
      * Initializes the interpolation method
+     *
      * @param a: Leftmost x-value of the grid
      * @param b: Rightmost x-value of the grid
      * @param y: Set of y values of the supporting points
