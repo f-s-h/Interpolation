80a9e6f96bdf585d6c99ae96899e40a56088ea56
f-s-h <fabianheinrich02@gmail.com>
2024-03-02 16:16:36 +0100

    Changed NewtonInterpolation: - changed arrays to list - added extendNewtonInterpolation

--- src/interpolation/NewtonInterpolation.java
+++ src/interpolation/NewtonInterpolation.java
@@ -1,14 +1,25 @@
 package interpolation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NewtonInterpolation extends InterpolationMethod {
     /**
      * Coefficients used to evaluate the
      */
-    double[] coefficients;
+    private ArrayList<Double> coefficients;
     /**
      * Grid points
      */
-    double[] x;
+    private ArrayList<Double> x;
+    /**
+     * y-Values at grid points
+     */
+    private ArrayList<Double> y;
+    /**
+     * Triangular scheme
+     */
+    private ArrayList<ArrayList<Double>> c;
 
     /**
      * @param a: Leftmost x-value of the grid
@@ -18,8 +29,7 @@ public class NewtonInterpolation extends InterpolationMethod {
     @Override
     public void init(double a, double b, double[] y) {
         super.init(a, b, y);
-        coefficients = new double[n + 1];
-        x = new double[n];
+        coefficients = new ArrayList<>();
         setGridPoints();
         computeCoefficients();
     }
@@ -28,8 +38,9 @@ public class NewtonInterpolation extends InterpolationMethod {
      * Sets the grid points
      */
     private void setGridPoints() {
+        x = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            x[i] = a + i * h;
+            x.add(a + i * h);
         }
     }
 
@@ -37,21 +48,47 @@ public class NewtonInterpolation extends InterpolationMethod {
      * computes the coefficients, using the triangular scheme
      */
     private void computeCoefficients() {
-        double[][] c = new double[n + 1][n + 1];
+        c = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            c.add(new ArrayList<>());
+        }
 
         // set c_i_0 = y_i
         for (int i = 0; i < n + 1; i++) {
-            c[i][0] = y[i];
+            c.get(i).add(y.get(i));
         }
         // set c_i_k = (c_i+1_k-1 - c_i_k-1)/(x_i+k - x_i)
         // fill the triangular scheme, the coefficients are in c[0][k]
         for (int k = 1; k < n + 1; k++) {
             for (int i = 0; i < n + 1 - k; i++) {
-                c[i][k] = (c[i + 1][k - 1] - c[i][k - 1]) / (x[i + k] - x[i]);
+                c.get(i).add((c.get(i + 1).get(k - 1) - c.get(i).get(k - 1)) / (x.get(i + k) - x.get(i)));
             }
         }
         // set the coefficients (coefficients are in c[0][k]
-        coefficients = c[0];
+        coefficients = c.get(0);
+    }
+
+    /**
+     * Extends the newton interpolation with a new grid point
+     * @param newX new x-value
+     * @param newY new y-value
+     */
+    public void extendNewtonInterpolation(double newX, double newY) {
+        // Extend triangular scheme
+        c.add(new ArrayList<>());
+        // Add new grid point
+        x.add(newX);
+        y.add(newY);
+        n++;
+
+        // add new y value at c_n_0
+        c.get(n).add(newY);
+
+        // extend triangular scheme, only the values on the new diagonal need to be set
+        for(int i = n - 1; n >= 0; n--) {
+            c.get(n-i).add((c.get(i + 1).get(n - 1) - c.get(i).get(n - 1)) / (x.get(i + n) - x.get(i)));
+        }
+
     }
 
     /**
@@ -66,9 +103,9 @@ public class NewtonInterpolation extends InterpolationMethod {
         double result = 0.;
 
         for (int i = 0; i < n + 1; i++) {
-            double temp_res = coefficients[i];
+            double temp_res = coefficients.get(i);
             for (int j = 0; j < i; j++) {
-                temp_res *= (z - x[j]);
+                temp_res *= (z - x.get(j));
             }
             result += temp_res;
         }
