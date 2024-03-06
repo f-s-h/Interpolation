92fd4cdcd2cfd1a22fb3fcefa436a94b94bba1b1
f-s-h <fabianheinrich02@gmail.com>
2024-03-06 18:34:13 +0100

    Fixed constructorTest()

--- test/interpolation/NewtonInterpolationTest.java
+++ test/interpolation/NewtonInterpolationTest.java
@@ -2,6 +2,7 @@ package interpolation;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,23 +16,30 @@ public class NewtonInterpolationTest {
         double a = 0.0;
         double b = 1.0;
         double[] y = new double[]{0., 1., 2., 3., 4.};
+        double[] x = new double[]{0, 0.25, 0.5, 0.75, 1.};
+
         NewtonInterpolation newtonInterpolation = new NewtonInterpolation();
         newtonInterpolation.init(a,b, y);
 
         double nExpected = 4;
         double hExpected = 0.25;
         ArrayList<Double> yExpected = new ArrayList<>(Arrays.stream(y).boxed().toList());
+        ArrayList<Double> xExpected = new ArrayList<>(Arrays.stream(x).boxed().toList());
 
         double aObserved = newtonInterpolation.a;
         double bObserved = newtonInterpolation.b;
         double nObserved = newtonInterpolation.n;
         double hObserved = newtonInterpolation.h;
         ArrayList<Double> yObserved = newtonInterpolation.getY();
+        ArrayList<Double> xObserved = newtonInterpolation.getX();
 
         assertEquals(a, aObserved, DELTA);
         assertEquals(b, bObserved, DELTA);
         assertEquals(nExpected, nObserved, DELTA);
         assertEquals(hExpected, hObserved, DELTA);
         assertTrue(yExpected.equals(yObserved));
+        assertTrue(xExpected.equals(xObserved));
     }
+
+
 }
